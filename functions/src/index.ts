0debf99e6eb7228f2c0cc301dcef384ffc9568b8
Ariel Ortuño Solis <arielg.os@gmail.com>
2024-10-22 22:16:11 -0400

    Adding new cloud function

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,7 +1,12 @@
 import { onRequest } from "firebase-functions/v2/https";
+import { onObjectFinalized } from "firebase-functions/storage";
 import * as logger from "firebase-functions/logger";
 
 export const helloWorld = onRequest((request, response) => {
   logger.info("Hello logs!", { structuredData: true });
   response.send("Hello from Firebase!");
+});
+
+export const callCustomModel = onObjectFinalized({ cpu: 2 }, async (event) => {
+  logger.log("callCustomModel", event);
 });
