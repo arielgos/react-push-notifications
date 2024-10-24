e021f54328f485b60a599a4b1ddb7e3d6a07a23b
Ariel Ortuño Solis <arielg.os@gmail.com>
2024-10-24 09:37:00 -0400

    minor improvements

--- public/src/components/Item.tsx
+++ public/src/components/Item.tsx
@@ -4,6 +4,7 @@ import { FC } from "react";
 import { Notification } from "../models/Models";
 import { getPublicUrl } from "../helpers/Firebase";
 import Image from "react-bootstrap/Image";
+import { Row, Col } from "react-bootstrap";
 
 interface ItemProps {
   notification: Notification;
@@ -14,15 +15,21 @@ const Item: FC<ItemProps> = (props) => {
     <Card>
       <Card.Header>{props.notification.title}</Card.Header>
       <Card.Body>
-        <Card.Title>{props.notification.message}</Card.Title>
-        <Card.Subtitle className="mb-2 text-muted">
-          <span className="text-muted small">
-            {Timestamp.fromMillis(props.notification.time).toDate().toUTCString()}
-          </span>
-        </Card.Subtitle>
-        <Card.Text>
-          <Image src={getPublicUrl(props.notification.file ?? "")} rounded />
-        </Card.Text>
+        <Row>
+          <Col>
+            <Card.Text>
+              <Image src={getPublicUrl(props.notification.file ?? "")} rounded width={"100%"} />
+            </Card.Text>
+          </Col>
+          <Col>
+            <Card.Title>{props.notification.message}</Card.Title>
+            <Card.Subtitle className="mb-2 text-muted">
+              <span className="text-muted small">
+                {Timestamp.fromMillis(props.notification.time).toDate().toUTCString()}
+              </span>
+            </Card.Subtitle>
+          </Col>
+        </Row>
       </Card.Body>
     </Card>
   );
