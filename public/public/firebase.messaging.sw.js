679cbb69c0b9439bca6dca5eea74268529a8eee9
Ariel Ortuño Solis <arielg.os@gmail.com>
2024-10-23 20:18:29 -0400

    Fixes for push notifications

--- public/public/firebase.messaging.sw.js
+++ public/public/firebase.messaging.sw.js
@@ -1,6 +1,6 @@
 importScripts('https://www.gstatic.com/firebasejs/9.2.0/firebase-app-compat.js');
 importScripts('https://www.gstatic.com/firebasejs/9.2.0/firebase-messaging-compat.js');
-importScripts('./firebase-config.js');
+importScripts('./firebase.config.js');
 
 firebase.initializeApp(firebaseConfig);
 
